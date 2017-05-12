b8920903fa885ca04c1e1343dfe73a6e03620833
akmalu21 <akmalu21@gmail.com>
2017-05-12 02:13:04 +0000

    Created Pages Controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def users
+  end
+
+  def drivers
+  end
+
+  def settings
+  end
+
+  def stats
+  end
+
+  def contacts
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+root 'pages#index'
+
+
+  get '/users' => 'pages#users'
+  get '/drivers' => 'pages#drivers'
+  get '/settings' => 'pages#settings'
+  get '/stats' => 'pages#stats'
+  get '/contacts' => 'pages#contacts'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
